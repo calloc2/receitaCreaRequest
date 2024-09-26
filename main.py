0c203ccdef2391f486212266e901918ff8a78539
Yuri <119751708+comatic0@users.noreply.github.com>
2024-09-26 15:37:16 -0300

    adds cnpj list in json with boolean for verification

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import time
 import requests
 import psycopg2
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,23 +15,45 @@ db_params = {
     "port": os.getenv("DB_PORT")
 }
 
+json_file = 'cnpj_list.json'
+
+if os.path.exists(json_file):
+    with open(json_file, 'r') as file:
+        cnpj_data = json.load(file)
+else:
+    cnpj_data = {}
+
 conn = psycopg2.connect(**db_params)
 cur = conn.cursor()
 
 cur.execute("SELECT cnpj FROM tb_empresa")
 cnpj_list = cur.fetchall()
-print("CNPJ List:", cnpj_list)
 cur.close()
 conn.close()
 
 for cnpj in cnpj_list:
     cnpj_code = cnpj[0]
-    if not cnpj_code:
+    if cnpj_code not in cnpj_data:
+        cnpj_data[cnpj_code] = {"requested": False}
+
+cnpj_codes_in_db = {cnpj[0] for cnpj in cnpj_list}
+cnpj_data = {cnpj_code: data for cnpj_code, data in cnpj_data.items() if cnpj_code in cnpj_codes_in_db}
+
+with open(json_file, 'w') as file:
+    json.dump(cnpj_data, file, indent=4)
+
+for cnpj_code, data in cnpj_data.items():
+    if data["requested"]:
         continue
     print(cnpj_code)
     url = f"https://receitaws.com.br/v1/cnpj/{cnpj_code}"
     print(url)
     response = requests.get(url)
     print(response.text)
+    
+    cnpj_data[cnpj_code]["requested"] = True
+    
+    with open(json_file, 'w') as file:
+        json.dump(cnpj_data, file, indent=4)
     
     time.sleep(20)
